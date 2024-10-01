b9120801d20801198b07c2b5c564048be7eedd3c
Anders Askeland <anders.askeland@gmail.com>
2024-10-01 08:43:33 +0200

    During presentation

--- R/analysis.R
+++ R/analysis.R
@@ -1,3 +1,4 @@
 do_analysis <- function(df) {
-
+  aov(formula = mpg ~ factor(cyl), data = df) |>
+    broom::tidy()
 }
--- R/plot.R
+++ R/plot.R
@@ -1,3 +1,4 @@
 make_plot <- function(df) {
-
+  df |> ggplot2::ggplot(ggplot2::aes(x = cyl, y = mpg)) +
+    ggplot2::geom_jitter()
 }
